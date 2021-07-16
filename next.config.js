2add625467e3e950148b4814248aaf172c85129f
Ritesh Kumar <ritesh@avika.in>
2021-07-16 23:59:40 +0530

    sdff
    fsdfsf

--- next.config.js
+++ next.config.js
@@ -8,7 +8,7 @@ const apiKey = JSON.stringify(process.env.SHOPIFY_API_KEY);
 module.exports = {
   webpack: (config) => {
     // configure API_KEY env variable needed for AppBridgeProvider component in _app.js
-    const env = { API_KEY: apiKey };
+    const env = { API_KEY: "3feaa822ea9422356b8b0e457ad57477" };
     config.plugins.push(new webpack.DefinePlugin(env));
 
     // Add ESM support for .mjs files in webpack 4
